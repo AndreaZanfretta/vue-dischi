9c318cbde4282afffa5b875bb5efa582422dc8f5
AndreaZanfretta <andrea.zanfretta.97@gmail.com>
2022-05-06 23:28:29 +0200

    aggiunto loader

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,6 +2,7 @@
     <main>
         <div class="container">
             <div class="row">
+                <app-loader v-if="loading" />
                 <div v-for="(disco, index) in dischi" :key="index" class="col-6 col-md-4 col-lg">
                     <app-card :disco="disco"/>
                 </div>
@@ -12,29 +13,34 @@
 
 <script>
 import AppCard from './AppCard.vue';
+import AppLoader from './AppLoader.vue';
 import axios from 'axios';
 
 export default {
     name: 'AppMain',
     components:{
         AppCard,
+        AppLoader,
     },
     data(){
         return{
             dischi: [],
             endPoint: "https://flynn.boolean.careers/exercises/api/array/",
+            loading: false,
         }
     },
     mounted(){
+        this.loading = true;
         axios.get(`${this.endPoint}music`)
         .then(res => {
             this.dischi = res.data.response;
-            console.log(this.dischi)
+            this.loading = false;
 
         })
         .catch(err => {
             console.error(err); 
-        })
+            this.loading = false;
+        })  
     }
     
 }
@@ -45,6 +51,7 @@ export default {
 @import "../assets/styles/vars";
 main{
     width: 100%;
+    min-height: calc(100vh - 80px);
     background-color: $main-bg;
 }
 .row{

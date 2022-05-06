709d85616ad2734562e095cd2b1781f1020886dc
AndreaZanfretta <andrea.zanfretta.97@gmail.com>
2022-05-06 15:50:35 +0200

    card iniziate

---
+++ src/components/AppCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="card-container">
+        <img :src="disco.poster" alt="disco.title">
+        <p id="title">{{disco.title}}</p>
+        <p>{{disco.author}}</p>
+        <p>{{disco.year}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppCard',
+    props: {
+        disco: Object,
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import "./public/styles/generals";
+@import "./public/styles/vars";
+.card-container{
+    width: 200px;
+    background-color: $header-bg;
+    margin: ;
+        img{
+            width: 100%;
+            padding: 20px 20px;
+        }
+        #title{
+            color: $title-color;
+        }
+        p{
+            margin: 0;
+        }
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,57 @@
 <template>
-    <div></div>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div v-for="(disco, index) in dischi" :key="index" class="col-6 col-md-4 col-lg-2">
+                    <app-card :disco="disco"/>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <script>
+import AppCard from './AppCard.vue';
+import axios from 'axios';
+
 export default {
     name: 'AppMain',
+    components:{
+        AppCard,
+    },
+    data(){
+        return{
+            dischi: [],
+            endPoint: "https://flynn.boolean.careers/exercises/api/array/",
+        }
+    },
+    mounted(){
+        axios.get(`${this.endPoint}music`)
+        .then(res => {
+            this.dischi = res.data.response;
+            console.log(this.dischi)
+
+        })
+        .catch(err => {
+            console.error(err); 
+        })
+    }
+    
 }
 </script>
 
 <style scoped lang="scss">
-
+@import "./public/styles/generals";
+@import "./public/styles/vars";
+main{
+    width: 100%;
+    height: calc(100vh - 80px);
+    background-color: $main-bg;
+}
+.container{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
 </style>

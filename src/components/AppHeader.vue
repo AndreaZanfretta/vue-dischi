ab69d3517cddc84c6ec1a8e11e3d2be72c9a7711
AndreaZanfretta <andrea.zanfretta.97@gmail.com>
2022-05-06 14:54:54 +0200

    scaffolding

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <div class="nav">
+            <img src="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-download-logo-30.png" alt="logo">
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader',
+}
+</script>
+
+<style scoped lang="scss">
+@import "./public/styles/generals";
+@import "./public/styles/vars";
+header{
+    width: 100%;
+    height: 80px;
+    background-color: $header-bg;
+}
+.nav{
+    width: 100%;
+    height: 100%;
+
+        img{
+            height: 100%;
+            padding: 10px 20px;
+        }
+}
+</style>
